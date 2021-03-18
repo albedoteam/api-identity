d513e45846d863e679b5140c6a237eb285049cd0
Eryckson Magno <eryckson@albedo.team>
2021-03-18 00:04:49 +0000

    Merged PR 219: username at provider

--- src/Models/User.cs
+++ src/Models/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using AlbedoTeam.Identity.Contracts.Common;
+using AlbedoTeam.Identity.Contracts.Responses;
 
 namespace Identity.Api.Models
 {
@@ -18,6 +19,7 @@ namespace Identity.Api.Models
         public List<string> Groups { get; set; }
         public Provider Provider { get; set; }
         public string ProviderId { get; set; }
+        public string UsernameAtProvider { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
         public bool IsDeleted { get; set; }
