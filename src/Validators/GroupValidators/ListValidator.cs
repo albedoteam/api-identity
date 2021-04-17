391f86e51b6df8115e560d04169724130ba12c4d
Eryckson Magno <eryckson@albedo.team>
2021-04-17 16:54:54 +0000

    Merged PR 290: remove pagination validation

--- src/Validators/GroupValidators/ListValidator.cs
+++ src/Validators/GroupValidators/ListValidator.cs
@@ -11,12 +11,6 @@
             RuleFor(c => c.AccountId)
                 .NotEmpty()
                 .Matches("^[0-9a-fA-F]{24}$", RegexOptions.IgnoreCase);
-
-            RuleFor(c => c.Page)
-                .GreaterThanOrEqualTo(1);
-
-            RuleFor(c => c.PageSize)
-                .GreaterThanOrEqualTo(1);
         }
     }
 }
