78e56d215244f441df04857b2920f857618bd31d
fiduta <eryckson@gmail.com>
2023-07-19 16:13:03 -0300

    just testing

---
+++ src/Controllers/JustTestController.cs
@@ -0,0 +1,13 @@
+﻿namespace Identity.Api.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class JustTestController: ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
